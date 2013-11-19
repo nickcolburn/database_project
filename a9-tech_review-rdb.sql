f3c5e4aba6c932c920ce87d186c756cb5baa70ab
Nick Colburn <nicholas.colburn92@gmail.com>
2013-11-18 21:18:39 -0800

    Update a9-tech_review-rdb.sql

--- a9-tech_review-rdb.sql
+++ a9-tech_review-rdb.sql
@@ -15,6 +15,34 @@ CREATE TABLE Retailers
   	name    VARCHAR(20) NOT NULL,
   	CONSTRAINT retailers_PK PRIMARY KEY(name) 
 );
+
+CREATE TABLE Components
+(	
+        name    	VARCHAR(20) NOT NULL,
+        type		VARCHAR(20),
+        specification 	VARCHAR(20),
+        CONSTRAINT retailers_PK PRIMARY KEY(name) 
+);
+  
+CREATE TABLE Users
+(
+	first_name	VARCHAR(20) NOT NULL,
+	last_name	VARCHAR(20) NOT NULL,
+	username	VARCHAR(20),
+);  
+
+CREATE TABLE Reviews
+(
+  	rating		DECIMAL(2,1),
+  	review		VARCHAR(1000),
+);
+
+CREATE TABLE Countries
+(
+	country_code	VARCHAR(3) NOT NULL,
+	name		VARCHAR(20),
+	CONSTRAINT countries_PK PRIMARY KEY(country_code)
+);
   
 CREATE TABLE Devices
 (
