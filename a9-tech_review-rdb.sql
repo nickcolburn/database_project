07baf77c848b043f2691c46765504df29af7e0b1
Nick Colburn <nicholas.colburn92@gmail.com>
2013-11-18 17:40:53 -0800

    Update a9-tech_review-rdb.sql

--- a9-tech_review-rdb.sql
+++ a9-tech_review-rdb.sql
@@ -3,3 +3,31 @@
 --          George Gutierrez
 --          Joao Leite
 
+--DROP TABLE device;
+CREATE TABLE Manufacturer
+(
+	name 	  VARCHAR(20) NOT NULL,
+	CONSTRAINT mfctr_PK PRIMARY KEY(name)
+);
+
+CREATE TABLE Retailers
+(
+  name    VARCHAR(20),
+  
+CREATE TABLE Devices
+(
+	name 		      VARCHAR(30) NOT NULL,
+	model_number	VARCHAR(30) NOT NULL,
+	listed_price	DECIMAL(5,2),
+	manufacturer	VARCHAR(30),
+	carrier		    VARCHAR(30),
+	weight		    VARCHAR(3), -- assumption: weight given is in ounces
+	release_date	DATE,
+	dimensions	  CHAR(8), -- (xx-xx-xx) format, in millimeters
+);
+
+CREATE TABLE Software
+(
+	name    VARCHAR(25),
+	version	DECIMAL(25)
+)
