ef5f68bf7242ccdad76edb0288f2c014379cea81
jfmartinelli <jfelipeleite@gmail.com>
2013-11-19 11:42:54 -0800

    Update a9-tech_review-rdb.sql

--- a9-tech_review-rdb.sql
+++ a9-tech_review-rdb.sql
@@ -13,7 +13,8 @@ CREATE TABLE Manufacturer
 CREATE TABLE Retailers
 (
   	name    	VARCHAR(20) NOT NULL,
-  	CONSTRAINT retailers_PK PRIMARY KEY(name) 
+  	region		VARCHAR(20), 
+  	CONSTRAINT retailers_PK PRIMARY KEY(name, region)
 );
 
 CREATE TABLE Components
@@ -24,9 +25,19 @@ CREATE TABLE Components
         CONSTRAINT retailers_PK PRIMARY KEY(name) 
 );
 
+CREATE TABLE Device_composition
+(
+	dev_id		INTEGER NOT NULL, -- FK from Devices
+	name 		VARCHAR(20) NOT NULL, -- FK from Components
+	CONSTRAINT dc_PK PRIMARY KEY(dev_id, name),
+	CONSTRAINT dc_devices_FK FOREIGN KEY(dev_id) REFERENCES Devices(dev_id),
+	CONSTRAINT dc_components_FK FOREIGN KEY(name) REFERENCES Components(name)
+	
+);
+
 CREATE TABLE Countries
 (
-	country_code	VARCHAR(3) NOT NULL,
+	country_code	CHAR(3) NOT NULL,
 	name		VARCHAR(20),
 	CONSTRAINT countries_PK PRIMARY KEY(country_code)
 );
@@ -41,13 +52,15 @@ CREATE TABLE Carriers
 CREATE TABLE Software
 (
 	name    	VARCHAR(20),
-	version		DECIMAL(3,1)
+	version		DECIMAL(3,1),
+	CONSTRAINT software_PK PRIMARY KEY(name, version)
 );
   
 CREATE TABLE Devices
 (
-	name 		VARCHAR(20) NOT NULL,
-	model_number	VARCHAR(20) NOT NULL,
+	dev_id 		INTEGER NOT NULL AUTO_INCREMENT,
+	name 		VARCHAR(20),
+	model_number	VARCHAR(20),
 	listed_price	DECIMAL(5,2),
 	manufacturer	VARCHAR(20),	-- FK from Manufacturers
 	software	VARCHAR(20),	-- FK from Software
@@ -55,19 +68,19 @@ CREATE TABLE Devices
 	weight		VARCHAR(3), 	-- assumption: weight given is in ounces
 	release_date	DATE,	
 	dimensions	CHAR(8), 	-- (xx-xx-xx) format, in millimeters
-	CONSTRAINT devices_PK PRIMARY KEY(name, model_number),
+	CONSTRAINT devices_PK PRIMARY KEY(dev_id),
 	CONSTRAINT devices_mfctr_FK FOREIGN KEY(manufacturer) REFERENCES Manufacturers(name),
 	CONSTRAINT devices_carrier_FK FOREIGN KEY(carrier) REFERENCES Carriers(name),
+	CONSTRAINT devices_software_FK FOREIGN KEY(software, version) REFERENCES Software(name, version)
 );
 
 CREATE TABLE Supports
 (
-	name		VARCHAR(20) NOT NULL,	-- FK from Devices 
-	model_number	VARCHAR(20) NOT NULL,	-- FK from Devices
+	dev_id 		INTEGER NOT NULL, --FK from Devices
 	carrier		VARCHAR(20) NOT NULL,	-- FK from Carriers
-	country		VARCHAR(3)  NOT NULL,	-- FK from Countries
-	CONSTRAINT supports_PK PRIMARY KEY(name, model_number, carrier, country),
-	CONSTRAINT supports_device_FK FOREIGN KEY(name, model_number) REFERENCES Devices(name, model_number),
+	country		CHAR(3)  NOT NULL,	-- FK from Countries
+	CONSTRAINT supports_PK PRIMARY KEY(dev_id, carrier, country),
+	CONSTRAINT supports_device_FK FOREIGN KEY(dev_id) REFERENCES Devices(dev_id),
 	CONSTRAINT supports_carrier_FK FOREIGN KEY(carrier) REFERENCES Carriers(name),
 	CONSTRAINT supports_country_FK FOREIGN KEY(country) REFERENCES Countries(country_code),
 );
@@ -75,9 +88,11 @@ CREATE TABLE Supports
 CREATE TABLE Sales
 (
 	retailer	VARCHAR(20) NOT NULL, 	-- FK from Retailers
-	name		VARCHAR(20) NOT NULL,	-- FK from Devices
-	model_number	VARCHAR(20) NOT NULL,	-- FK from Devices
+	dev_id 		INTEGER NOT NULL, -- FK from Devices
 	sale_price	DECIMAL(5,2) NOT NULL,
+	CONSTRAINT sales_PK PRIMARY KEY(dev_id, retailer),
+	CONSTRAINT sales_device_FK FOREIGN KEY(dev_id) REFERENCES Devices(dev_id),
+	CONSTRAINT sales_retailer_FK FOREIGN KEY(retailer) REFERENCES Retailers(name)
 	
 );
 
@@ -86,18 +101,18 @@ CREATE TABLE Users
 	username	VARCHAR(20) NOT NULL,
 	first_name	VARCHAR(20),
 	last_name	VARCHAR(20),
-	CONSTRAINT USERS_PK PRIMARY KEY(username)
+	CONSTRAINT users_PK PRIMARY KEY(username)
 );  
 
 CREATE TABLE Reviews
 (
- 	name		VARCHAR(20),	-- FK from Devices
-  	model_number	VARCHAR(20),	-- FK from Devices
+ 	dev_id 		INTEGER NOT NULL, --FK from Devices
   	username	VARCHAR(20) NOT NULL,	-- CK & FK from Users
   	rating		DECIMAL(2,1),	-- users can give partial ratings
   	review		VARCHAR(1000),	-- need to determine appropriate size for this
-  	CONTRAINT reviews_PK PRIMARY KEY(username) REFERNCES Users(username),
-  	CONSRAINT reviews_device_FK FOREIGN KEY(name, model_number) REFERENCES Devices(name, model_number),
+  	CONSTRAINT reviews_PK PRIMARY KEY(username),
+  	CONSTRAINT reviews_user_FM FOREIGN KEY(username) REFERENCES Users(username),
+  	CONSTRAINT reviews_device_FK FOREIGN KEY(name, model_number) REFERENCES Devices(name, model_number),
 );
 
 
