a6d93a1df1d846bb90b26333f33c2848e6042593
Nick Colburn <nicholas.colburn92@gmail.com>
2013-11-21 14:30:42 -0800

    Update a9-tech_review-rdb.sql

--- a9-tech_review-rdb.sql
+++ a9-tech_review-rdb.sql
@@ -1,7 +1,7 @@
 -- Assignment 9 - Tech Review Relational Database
 -- Authors: Nick Colburn
---          George Gutierrez
---          Joao Leite
+-- George Gutierrez
+-- Joao Leite
 
 DROP TABLE Reviews;
 DROP TABLE Users;
@@ -13,67 +13,68 @@ DROP TABLE Software;
 DROP TABLE Carriers;
 DROP TABLE Countries;
 DROP TABLE Components;
+DROP TABLE Retailers;
 DROP TABLE Manufacturers;
 
 
 CREATE TABLE Manufacturers
 (
-        name                   VARCHAR(20) NOT NULL,
+        name VARCHAR(20) NOT NULL,
         CONSTRAINT mfctr_PK PRIMARY KEY(name)
 );
 
 CREATE TABLE Retailers
 (
-          name            VARCHAR(20) NOT NULL,
-          region                VARCHAR(20), 
+          name VARCHAR(20) NOT NULL,
+          region VARCHAR(20),
           CONSTRAINT retailers_PK PRIMARY KEY(name, region)
 );
 
 CREATE TABLE Components
-(        
-        comp_id                INTEGER NOT NULL AUTO_INCREMENT,
-        name            VARCHAR(20),
-        comp_type        VARCHAR(20),        -- component type (type must be an SQL keyword)
-        specification         VARCHAR(20),
-        CONSTRAINT retailers_PK PRIMARY KEY(comp_id) 
+(
+        comp_id INTEGER NOT NULL AUTO_INCREMENT,
+        name VARCHAR(20),
+        comp_type VARCHAR(20), -- component type (type must be an SQL keyword)
+        specification VARCHAR(20),
+        CONSTRAINT retailers_PK PRIMARY KEY(comp_id)
 );
 
 
 CREATE TABLE Countries
 (
-        country_code        CHAR(3) NOT NULL,
-        name                VARCHAR(20),
+        country_code CHAR(3) NOT NULL,
+        name VARCHAR(20),
         CONSTRAINT countries_PK PRIMARY KEY(country_code)
 );
 
 CREATE TABLE Carriers
 (
-        name            VARCHAR(20) NOT NULL,
-        radio_spectrum        VARCHAR(10),
+        name VARCHAR(20) NOT NULL,
+        radio_spectrum VARCHAR(10),
         CONSTRAINT carriers_PK PRIMARY KEY(name)
 );
 
 CREATE TABLE Software
 (
-        name            VARCHAR(20),
-        version                DECIMAL(3,1),
+        name VARCHAR(20),
+        version DECIMAL(3,1),
         CONSTRAINT software_PK PRIMARY KEY(name, version)
 );
   
 CREATE TABLE Devices
 (
-        dev_id          INTEGER NOT NULL AUTO_INCREMENT
-        name            VARCHAR(20),
-        model_number    VARCHAR(20),
-        listed_price    DECIMAL(5,2),   -- assumption: listed price is full retail price 
-        manufacturer    VARCHAR(20),    -- FK from Manufacturers
-        software        VARCHAR(20),    -- FK from Software
-        version         DECIMAL(3,1),   -- FK software version from Software
-        weight          DECIMAL(2,1),   -- assumption: weight given is in ounces
-        release_date    DATE,        
-        height          DECIMAL(4,1),   -- assumption: values are in millimeters
-        width           DECIMAL(3,1),   -- assumption: values are in millimeters
-        depth           DECIMAL(3,1),   -- assumption: values are in millimeters
+        dev_id INTEGER NOT NULL AUTO_INCREMENT,
+        name VARCHAR(20),
+        model_number VARCHAR(20),
+        listed_price DECIMAL(5,2), -- assumption: listed price is full retail price
+        manufacturer VARCHAR(20), -- FK from Manufacturers
+        software VARCHAR(20), -- FK from Software
+        version DECIMAL(3,2), -- FK software version from Software
+        weight DECIMAL(3,2), -- assumption: weight given is in ounces
+        release_date DATE,
+        height DECIMAL(4,2), -- assumption: values are in millimeters
+        width DECIMAL(3,2), -- assumption: values are in millimeters
+        depth DECIMAL(3,2), -- assumption: values are in millimeters
         CONSTRAINT devices_PK PRIMARY KEY(dev_id),
         CONSTRAINT devices_mfctr_FK FOREIGN KEY(manufacturer) REFERENCES Manufacturers(name) ON UPDATE CASCADE ON DELETE CASCADE,
         CONSTRAINT devices_software_FK FOREIGN KEY(software, version) REFERENCES Software(name, version) ON UPDATE CASCADE ON DELETE CASCADE
@@ -81,8 +82,8 @@ CREATE TABLE Devices
 
 CREATE TABLE Device_composition
 (
-        dev_id          INTEGER NOT NULL, -- FK from Devices
-        comp_id         INTEGER NOT NULL, -- FK from Components
+        dev_id INTEGER NOT NULL, -- FK from Devices
+        comp_id INTEGER NOT NULL, -- FK from Components
         CONSTRAINT dc_PK PRIMARY KEY(dev_id, comp_id),
         CONSTRAINT dc_devices_FK FOREIGN KEY(dev_id) REFERENCES Devices(dev_id) ON UPDATE CASCADE ON DELETE CASCADE,
         CONSTRAINT dc_components_FK FOREIGN KEY(comp_id) REFERENCES Components(comp_id) ON UPDATE CASCADE ON DELETE CASCADE
@@ -91,9 +92,9 @@ CREATE TABLE Device_composition
 
 CREATE TABLE Supports
 (
-        dev_id                 INTEGER NOT NULL,         --FK from Devices
-        carrier                VARCHAR(20) NOT NULL,     -- FK from Carriers
-        country                CHAR(3)  NOT NULL,        -- FK from Countries
+        dev_id INTEGER NOT NULL, -- FK from Devices
+        carrier VARCHAR(20) NOT NULL, -- FK from Carriers
+        country CHAR(3) NOT NULL, -- FK from Countries
         CONSTRAINT supports_PK PRIMARY KEY(dev_id, carrier, country),
         CONSTRAINT supports_device_FK FOREIGN KEY(dev_id) REFERENCES Devices(dev_id) ON UPDATE CASCADE ON DELETE CASCADE,
         CONSTRAINT supports_carrier_FK FOREIGN KEY(carrier) REFERENCES Carriers(name) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -102,9 +103,9 @@ CREATE TABLE Supports
 
 CREATE TABLE Sales
 (
-        retailer        VARCHAR(20) NOT NULL,   -- FK from Retailers
-        dev_id          INTEGER NOT NULL,       -- FK from Devices
-        sale_price      DECIMAL(5,2) NOT NULL,
+        retailer VARCHAR(20) NOT NULL, -- FK from Retailers
+        dev_id INTEGER NOT NULL, -- FK from Devices
+        sale_price DECIMAL(5,2) NOT NULL,
         CONSTRAINT sales_PK PRIMARY KEY(dev_id, retailer),
         CONSTRAINT sales_device_FK FOREIGN KEY(dev_id) REFERENCES Devices(dev_id) ON UPDATE CASCADE ON DELETE CASCADE,
         CONSTRAINT sales_retailer_FK FOREIGN KEY(retailer) REFERENCES Retailers(name) ON UPDATE CASCADE ON DELETE CASCADE
@@ -113,21 +114,21 @@ CREATE TABLE Sales
 
 CREATE TABLE Users
 (
-        username        VARCHAR(20) NOT NULL,
-        first_name        VARCHAR(20),
-        last_name        VARCHAR(20),
+        username VARCHAR(20) NOT NULL,
+        first_name VARCHAR(20),
+        last_name VARCHAR(20),
         CONSTRAINT users_PK PRIMARY KEY(username)
-);  
+);
 
 CREATE TABLE Reviews
 (
-        dev_id          INTEGER NOT NULL AUTO_INCREMENT,        --FK from Devices
-        username        VARCHAR(20) NOT NULL,                   -- FK from Users
-        rating          DECIMAL(2,1),                           -- users can give partial ratings
-        review          VARCHAR(1000),                          -- need to determine appropriate size for this
+        dev_id INTEGER NOT NULL AUTO_INCREMENT, -- FK from Devices
+        username VARCHAR(20) NOT NULL, -- FK from Users
+        rating DECIMAL(2,1), -- users can give partial ratings
+        review VARCHAR(1000), -- need to determine appropriate size for this
         CONSTRAINT reviews_PK PRIMARY KEY(dev_id, username),
-        CONSTRAINT reviews_device_FK FOREIGN KEY(dev_id) REFERENCES Devices(dev_id) ON UPDATE CASCADE ON DELETE CASCADE
-        CONSTRAINT reviews_user_FK FOREIGN KEY(username) REFERENCES Users(username) ON UPDATE CASCADE ON DELETE CASCADE,
+        CONSTRAINT reviews_device_FK FOREIGN KEY(dev_id) REFERENCES Devices(dev_id) ON UPDATE CASCADE ON DELETE CASCADE,
+        CONSTRAINT reviews_user_FK FOREIGN KEY(username) REFERENCES Users(username) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 INSERT INTO Manufacturers(name) VALUES
@@ -152,13 +153,13 @@ INSERT INTO Components(name, comp_type) VALUES
         ('Snapdragon 600', 'CPU');
 
 INSERT INTO Countries(country_code, name) VALUES
-        ('USA', 'United States of America'),
+        ('USA', 'United States'),
         ('MEX', 'Mexico'),
         ('FRA', 'France'),
         ('GBR', 'United Kingdom'),
         ('CAN', 'Canada');
 
-INSERT INTO Carries(name, radio_spectrum) VALUES
+INSERT INTO Carriers(name, radio_spectrum) VALUES
         ('Verizon Wireless', 'CDMA'),
         ('Sprint', 'CDMA'),
         ('AT&T', 'GSM'),
@@ -175,7 +176,7 @@ INSERT INTO Software(name, version) VALUES
 -- Listed price needs to be the off-contract price
 INSERT INTO Devices(name, model_number, listed_price, release_date, weight, height, width, depth) VALUES
         ('Galaxy S4', 'GT-I9505', 637.99, '2013-04-26', 4.6, 136.6, 69.8, 7.9),
-        ('Galaxy S3', 'I9300', 569.99, 4.1, '2012-05-01', 4.6, 136.6, 70.6, 8.6),
+        ('Galaxy S3', 'I9300', 569.99, '2012-05-01', 4.6, 136.6, 70.6, 8.6),
         ('DROID MAXX', 'XT1080', 499.00, '2013-07-23', 4.48, 130.7, 68.9, 7.1),
         ('Nexus 5 (16GB)', 'LG-D820', 349.00, '2013-10-31', 4.59, 137.9, 69.2, 8.6),
         ('iPhone 5S (16GB)', 'A1533', 649.99, '2013-09-20', 3.95, 123.8, 58.6, 7.6);
@@ -200,12 +201,3 @@ INSERT INTO Review(rating, review) VALUES
         (4.0, 'I love the long lasting battery on the RAZR MAXX. No more needing to carry a charger around for this guy!'),
         (4.0, 'Kit Kat brings some fun cool new features to the phone. The only downside is that the camera struggles under auto setting.'),
         (4.5, 'Amazing phone! I only wish that the cheap plastic casing makes it feel and look like a cheap phone.');
-        
-        
-        
-        
-        
-        
-        
-        
-        
