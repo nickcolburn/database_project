d4558674f9bb11bd3d05fe9712c189ac5d290027
Nick Colburn <nicholas.colburn92@gmail.com>
2013-12-05 12:30:51 -0800

    Update a9-tech_review-create_tables.sql

--- a9-tech_review-create_tables.sql
+++ a9-tech_review-create_tables.sql
@@ -21,15 +21,19 @@ DROP TABLE Manufacturers;
 
 CREATE TABLE Manufacturers
 (
-        name		VARCHAR(20) NOT NULL,
-        CONSTRAINT mfctr_pk PRIMARY KEY(name) -- there is only 1 attribute in this table so it can serve as the PK
+        mfctr_id	INTEGER NOT NULL AUTO_INCREMENT, -- Surrogate PK for Manufacturer
+        name		VARCHAR(20),
+        CONSTRAINT 	mfctr_pk PRIMARY KEY(mfctr_id),-- mfctr_id is a simpler Pk than name, while still determining the same attributes
+	CONSTRAINT	mfctr_ck UNIQUE(name)
 );
 
 CREATE TABLE Retailers
 (
-	name 		VARCHAR(20) NOT NULL,
-        region 		VARCHAR(20) NOT NULL,
-        CONSTRAINT retailers_pk PRIMARY KEY(name, region) -- both name+region will be the PK to distinguish any retailers with the same name in different regions
+	retailer_id	INTEGER NOT NULL AUTO_INCREMENT,
+	name 		VARCHAR(20),
+        region 		VARCHAR(20),
+        CONSTRAINT 	retailers_pk PRIMARY KEY(retailer_id) -- both name+region will be the PK to distinguish any retailers with the same name in different regions
+	CONSTRAINT	retailers_ck UNIQUE(name, region) -- name+region is used to determine retailers present in 2+ countries
 );
 
 CREATE TABLE Components
@@ -61,15 +65,17 @@ CREATE TABLE Radio_spectrum
 (
 	carrier_name	VARCHAR(20) NOT NULL, -- FK from Carriers
 	radio_spectrum	VARCHAR(20) NOT NULL,
-	CONSTRAINT 	radio_spectrum_pk PRIMARY KEY(name, radio_spectrum),
+	CONSTRAINT 	radio_spectrum_pk PRIMARY KEY(carrier_name, radio_spectrum),
 	CONTRAINT 	radio_carriers_fk FOREIGN KEY(carrier_name) REFERENCES Carriers(name)
 );
 
 CREATE TABLE Software
 (
+        soft_id		INTEGER NOT NULL AUTO_INCREMENT,
         name 		VARCHAR(20) NOT NULL,
-        version 	VARCHAR(20) NOT NULL, -- assumption: version number will be in XX.X format or named
-        CONSTRAINT 	software_pk PRIMARY KEY(name, version) -- name and version must be used because there may be several iterations of software with the same name. 
+        version 	VARCHAR(20) NOT NULL, -- assumption: version number will be named or in XX.X format 
+        CONSTRAINT 	software_pk PRIMARY KEY(soft_id), -- software id will determine name and ver. # 
+        CONSTRAINT 	software_ck UNIQUE(name, version) -- name and version must be used because there may be several iterations of software with the same name. 
 );
   
 CREATE TABLE Devices
@@ -78,8 +84,8 @@ CREATE TABLE Devices
         name 		VARCHAR(20),
         model_number 	VARCHAR(20),
         listed_price 	DECIMAL(5,2), 	-- assumption: listed price is full retail price
-        manufacturer 	VARCHAR(20), 	-- FK from Manufacturers
-        software_name 	VARCHAR(20), 	-- FK from Software
+        mfctr_id 	VARCHAR(20), 	-- FK from Manufacturers
+        soft_id 	VARCHAR(20), 	-- FK from Software
         version 	VARCHAR(20),	-- FK software version from Software
         weight 		DECIMAL(3,2), 	-- assumption: weight given is in ounces
         release_date 	DATE,
@@ -87,8 +93,8 @@ CREATE TABLE Devices
         width 		DECIMAL(3,1), 	-- assumption: values are in millimeters
         depth 		DECIMAL(3,1),	-- assumption: values are in millimeters
         CONSTRAINT devices_pk PRIMARY KEY(dev_id), -- dev_id is a surrogate key
-        CONSTRAINT devices_mfctr_fk FOREIGN KEY(manufacturer) REFERENCES Manufacturers(name) ON UPDATE CASCADE ON DELETE RESTRICT,
-        CONSTRAINT devices_software_fk FOREIGN KEY(software_name, version) REFERENCES Software(name, version) ON UPDATE CASCADE ON DELETE RESTRICT,
+        CONSTRAINT devices_mfctr_fk FOREIGN KEY(mfctr_id) REFERENCES Manufacturers(mfctr_id) ON UPDATE CASCADE ON DELETE RESTRICT,
+        CONSTRAINT devices_software_fk FOREIGN KEY(soft_id) REFERENCES Software(soft_id) ON UPDATE CASCADE ON DELETE RESTRICT,
         CONSTRAINT devices_ck UNIQUE(name, model_number)
 );
 
@@ -122,12 +128,12 @@ CREATE TABLE Supports
 
 CREATE TABLE Sales
 (
-        retailer 	VARCHAR(20) NOT NULL, 	-- FK from Retailers
+        retailer_id	INTEGER NOT NULL, -- FK from Retailers
         dev_id 		INTEGER NOT NULL, 	-- FK from Devices
         sale_price 	DECIMAL(5,2) NOT NULL,
         CONSTRAINT 	sales_pk PRIMARY KEY(dev_id, retailer),
         CONSTRAINT 	sales_device_fk FOREIGN KEY(dev_id) REFERENCES Devices(dev_id) ON UPDATE CASCADE ON DELETE CASCADE,
-        CONSTRAINT 	sales_retailer_fk FOREIGN KEY(retailer) REFERENCES Retailers(name) ON UPDATE CASCADE ON DELETE CASCADE
+        CONSTRAINT 	sales_retailer_fk FOREIGN KEY(retailer) REFERENCES Retailers(retailer_id) ON UPDATE CASCADE ON DELETE CASCADE
         
 );
 
