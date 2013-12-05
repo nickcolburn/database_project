0400d1cfcb1a83312f0eec46852296355bda6911
Nick Colburn <nicholas.colburn92@gmail.com>
2013-12-05 12:58:47 -0800

    Update a10-tech_review-inserts.sql

--- a10-tech_review-inserts.sql
+++ a10-tech_review-inserts.sql
@@ -68,7 +68,6 @@ INSERT INTO Carriers(name, radio_spectrum) VALUES
         ('America Movil', 'tbd'),
         ('Telefonica', 'tbd'),
         ('Orange', 'tbd'),
-        ('Reliance Communications', 'tbd'),
         ('MTS', 'tbd'),
         ('BSNL', 'tbd');
         
