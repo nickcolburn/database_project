fdc3cb3a5de80e0c560dfd97156d7709b2699433
George91340 <gmgutierrez87@gmail.com>
2013-12-10 12:20:20 -0800

    Update a10-tech_review-inserts.sql
    added comments to Components inserts, Devices inserts, Software inserts.
    
    added inserts to internals and devices table

--- a10-tech_review-inserts.sql
+++ a10-tech_review-inserts.sql
@@ -5,14 +5,14 @@
 
 
 INSERT INTO Manufacturers(name) VALUES
-        ('Samsung'),
-        ('Apple'),
-        ('Motorola'),
-        ('LG'),
-        ('Sony'),
-        ('RIM'),
-        ('Nokia'),
-        ('HTC');
+        ('Samsung'), -- mfctr_id = 1
+        ('Apple'), -- mfctr_id = 2
+        ('Motorola'), -- mfctr_id = 3
+        ('LG'), -- mfctr_id = 4
+        ('Sony'), -- mfctr_id = 5
+        ('RIM'), -- mfctr_id = 6
+        ('Nokia'), -- mfctr_id = 7
+        ('HTC'); -- mfctr_id = 8
 
 INSERT INTO Retailers(name, region) VALUES
         ('Best Buy', 'North America'),
@@ -22,11 +22,28 @@ INSERT INTO Retailers(name, region) VALUES
         ('Apple', 'North America');
         
 INSERT INTO Components(name, comp_type) VALUES
-        ('13 MP', 'camera'),
-        ('2600 mAh Lithium Ion', 'Battery'),
-        ('HD Super AMOLED', 'Screen'),
-        ('2 GB LPDDR3', 'Memory'),
-        ('Snapdragon 600', 'CPU');
+	-- RAM sizes
+	('2 GB DDR3', 'Memory'), -- comp_id = 1 (Galaxy S4,)
+	('1 GB DDR3', 'RAM'), -- comp_id = 2 (iPhone 5s, )
+	
+	-- Cameras
+	('13 MP', 'Rear Camera'), -- comp_id = 3 (Galaxy S4,)
+	('8 MP', 'Rear Camera'), -- comp_id = 4 (iPhone 5s, )
+	('2 Megapixels', 'Front Camera'), -- comp_id = 5 (Galaxy S4,)
+	('1.2 MP' 'Front Camera'), -- comp_id = 6 (iPhone 5s, )
+        
+        -- specific components for Galaxy S4
+        ('2600 mAh', 'Battery'), -- comp_id = 7
+        ('HD Super AMOLED', 'Display'), -- comp_id = 8
+	('Qualcomm Snapdragon 600', 'Processor'), -- comp_id = 9
+
+	-- components for the iPhone 5s
+	('A7', 'Processor'), -- comp_id = 10
+	('Retina Display', 'Display'), -- comp_id = 11
+	('Touch ID', 'Fingerprint Sensor'), -- comp_id = 12
+	('1560 mAh', 'Battery'); -- comp_id = 13
+	
+	
 
 INSERT INTO Countries(country_code, name) VALUES
         ('AUS', 'Australia'),
@@ -70,21 +87,28 @@ INSERT INTO Carriers(name, country_code) VALUES
         ('BSNL', 'MEX');
         
 INSERT INTO Software(name, version) VALUES
-        ('Android', 'KitKat'),
-        ('Android', 'Jelly Bean'),
-        ('Android', 'Ice Cream Sandwich'),
-        ('iOS', '7.0'),
-        ('iOS', '6.0');
+	('Android', 'KitKat'), -- soft_id = 1
+        ('Android', 'Jelly Bean'), -- soft_id = 2
+        ('Android', 'Ice Cream Sandwich'), -- soft_id = 3
+        ('iOS', '7.0'), -- soft_id = 4
+        ('iOS', '6.0'); -- soft_id = 5
 
 INSERT INTO Devices(name, model_number, listed_price, mfctr_id, soft_id, release_date, weight, height, width, depth) VALUES
-        -- mfcrt_id 1 = Samsung, 2=Apple, 3=Motorola, 4=LG
-        -- soft_id 1= Android KitKat, 2=Android Jelly Bean, 3=Android Ice Cream Sandwich, 4=iOS7, 5=iOS6
+        -- dev_id = 1
         ('Galaxy S4',           'GT-I9505',     637.99, (SELECT mfctr_id FROM Manufacturers WHERE mfctr_id=1) , (SELECT soft_id FROM Software WHERE soft_id=3),'2013-04-26', 4.6,  136.6, 69.8, 7.9),
+        -- dev_id = 2
         ('Galaxy S3',           'I9300',        569.99, (SELECT mfctr_id FROM Manufacturers WHERE mfctr_id=1) , (SELECT soft_id FROM Software WHERE soft_id=2),'2012-05-01', 4.6,  136.6, 70.6, 8.6),
+        -- dev_id = 3
         ('DROID MAXX',          'XT1080',       499.00, (SELECT mfctr_id FROM Manufacturers WHERE mfctr_id=3) , (SELECT soft_id FROM Software WHERE soft_id=2),'2013-07-23', 4.48, 130.7, 68.9, 7.1),
+        -- dev_id = 4
         ('Nexus 5 (16GB)',      'LG-D820',      349.00, (SELECT mfctr_id FROM Manufacturers WHERE mfctr_id=4) , (SELECT soft_id FROM Software WHERE soft_id=3),'2013-10-31', 4.59, 137.9, 69.2, 8.6),
-        ('iPhone 5S (16GB)',     'A1533',       649.99, (SELECT mfctr_id FROM Manufacturers WHERE mfctr_id=2) , (SELECT soft_id FROM Software WHERE soft_id=4),'2013-09-20', 3.95, 123.8, 58.6, 7.6);
-        
+        -- dev_id = 5
+        ('iPhone 5S (16GB)',     'A1533',       649.99, (SELECT mfctr_id FROM Manufacturers WHERE mfctr_id=2) , (SELECT soft_id FROM Software WHERE soft_id=4),'2013-09-20', 3.95, 123.8, 58.6, 7.6),
+        -- dev_id = 6
+        ('iPhone 5S (32GB)',     'A1533',       649.99, (SELECT mfctr_id FROM Manufacturers WHERE mfctr_id=2) , (SELECT soft_id FROM Software WHERE soft_id=4),'2013-09-20', 3.95, 123.8, 58.6, 7.6),
+        -- dev_id = 7
+        ('iPhone 5S (64GB)',     'A1533',       649.99, (SELECT mfctr_id FROM Manufacturers WHERE mfctr_id=2) , (SELECT soft_id FROM Software WHERE soft_id=4),'2013-09-20', 3.95, 123.8, 58.6, 7.6);
+
 INSERT INTO Sales(dev_id, retailer_id, sale_price) VALUES
         ((SELECT dev_id FROM Devices WHERE dev_id=1), (SELECT retailer_id FROM Retailers WHERE name='Amazon'),          199.99),
         ((SELECT dev_id FROM Devices WHERE dev_id=2), (SELECT retailer_id FROM Retailers WHERE name='Best Buy'),        0.0),
@@ -120,7 +144,7 @@ INSERT INTO Supports(dev_id, country, carrier) VALUES
         ((SELECT dev_id FROM Devices WHERE dev_id=5), (SELECT country_code FROM Countries WHERE name = 'United States'), (SELECT name FROM Carriers WHERE name = 'Verizon Wireless')),
         ((SELECT dev_id FROM Devices WHERE dev_id=1), (SELECT country_code FROM Countries WHERE name = 'United States'), (SELECT name FROM Carriers WHERE name = 'Orange'));
     
-INSERT INTO Radio_spectrum(carrier_name, radio_spectrum) VALUE
+INSERT INTO Radio_spectrum(carrier_name, radio_spectrum) VALUES
 	((SELECT name FROM Devices WHERE name = 'Verizon'), 'CDMA'),
 	((SELECT name FROM Devices WHERE name = 'AT&T'), 'GSM'),
 	((SELECT name FROM Devices WHERE name = 'T-Mobile'), 'GSM'),
@@ -139,3 +163,28 @@ INSERT INTO Radio_spectrum(carrier_name, radio_spectrum) VALUE
 	((SELECT name FROM Devices WHERE name = 'Orange'), 'GSM'),
 	((SELECT name FROM Devices WHERE name = 'MTS'), 'GSM'), -- RUSSIA
 	((SELECT name FROM Devices WHERE name = 'BSNL'), 'GSM'); -- INDIA
+
+INSERT INTO Internals(dev_id, comp_id) VALUES
+	-- the following internals are for the Samsung Galaxy S4
+	((SELECT name = FROM Devices WHERE dev_id = '1'),(SELECT name FROM Components WHERE comp_id = '1'),
+	((SELECT name = FROM Devices WHERE dev_id = '1'),(SELECT name FROM Components WHERE comp_id = '3'), 
+	((SELECT name = FROM Devices WHERE dev_id = '1'),(SELECT name FROM Components WHERE comp_id = '5'), 
+	((SELECT name = FROM Devices WHERE dev_id = '1'),(SELECT name FROM Components WHERE comp_id = '7'), 
+	((SELECT name = FROM Devices WHERE dev_id = '1'),(SELECT name FROM Components WHERE comp_id = '8'),
+	((SELECT name = FROM Devices WHERE dev_id = '1'),(SELECT name FROM Components WHERE comp_id = '9'),
+	-- the following internals are for the iPhone 5s (16 GB)
+	((SELECT name = FROM Devices WHERE dev_id = '5'),(SELECT name FROM Components WHERE comp_id = '2'),
+	((SELECT name = FROM Devices WHERE dev_id = '5'),(SELECT name FROM Components WHERE comp_id = '4'), 
+	((SELECT name = FROM Devices WHERE dev_id = '5'),(SELECT name FROM Components WHERE comp_id = '6'), 
+	((SELECT name = FROM Devices WHERE dev_id = '5'),(SELECT name FROM Components WHERE comp_id = '10'), 
+	((SELECT name = FROM Devices WHERE dev_id = '5'),(SELECT name FROM Components WHERE comp_id = '11'),
+	((SELECT name = FROM Devices WHERE dev_id = '5'),(SELECT name FROM Components WHERE comp_id = '12'),
+	((SELECT name = FROM Devices WHERE dev_id = '5'),(SELECT name FROM Components WHERE comp_id = '13'),
+	-- the following internals are for the iPhone 5s (32 GB)
+	((SELECT name = FROM Devices WHERE dev_id = '6'),(SELECT name FROM Components WHERE comp_id = '2'),
+	((SELECT name = FROM Devices WHERE dev_id = '6'),(SELECT name FROM Components WHERE comp_id = '4'), 
+	((SELECT name = FROM Devices WHERE dev_id = '6'),(SELECT name FROM Components WHERE comp_id = '6'), 
+	((SELECT name = FROM Devices WHERE dev_id = '6'),(SELECT name FROM Components WHERE comp_id = '10'), 
+	((SELECT name = FROM Devices WHERE dev_id = '6'),(SELECT name FROM Components WHERE comp_id = '11'),
+	((SELECT name = FROM Devices WHERE dev_id = '6'),(SELECT name FROM Components WHERE comp_id = '12'),
+	((SELECT name = FROM Devices WHERE dev_id = '6'),(SELECT name FROM Components WHERE comp_id = '13');
