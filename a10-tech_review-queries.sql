62a888073c79372951ffea96b6d3fa9af7aa9a26
jfmartinelli <jfelipeleite@gmail.com>
2013-12-10 15:16:18 -0800

    Update a10-tech_review-queries.sql

--- a10-tech_review-queries.sql
+++ a10-tech_review-queries.sql
@@ -33,4 +33,30 @@ SELECT d.name, d.model_number, s.sale_price, COUNT(s.dev_id) AS Qty_sold, r.name
 		LEFT OUTER JOIN Sales s ON s.dev_id = d.dev_id
 		LEFT OUTER JOIN Retailers r ON s.retailer_id = r.retailer_id
 GROUP BY d.name;
-		 
+
+
+-- ## 3 NEW QUERIES ##
+
+-- Show devices for a given value for rating. It shows 
+SELECT r.rating AS Rating, d.name as Device_Name, d.model_number as Model_Number, m.name as Manufacturer_Name, s.name as Software_Name FROM Reviews r
+	NATURAL JOIN Devices d
+	LEFT OUTER JOIN Manufacturers m ON d.mfctr_id = m.mfctr_id
+	LEFT OUTER JOIN Software s ON d.soft_id = s.soft_id
+WHERE r.rating = 4.0; -- Replace 4.0 for the float value retrieved by the Java textfield
+
+
+-- query to show devices based on screen size - the diagonal size can be found using height and width.
+SELECT d.name, d.model_number AS model_number, m.name AS manufacturer_name, s.name AS software_name, d.height, d.width, SQRT(d.height*d.height + d.width*d.width) AS diagonal_size
+FROM Devices d
+	LEFT OUTER JOIN Manufacturers m ON d.mfctr_id = m.mfctr_id
+	LEFT OUTER JOIN Software s ON d.soft_id = s.soft_id
+GROUP BY diagonal_size
+HAVING diagonal_size > 150.00; -- Replace 150 for the float value retrieved by the Java textfield
+
+	
+-- query to show devices based on price.
+SELECT d.name, d.model_number AS model_number, m.name AS manufacturer_name, s.name AS software_name, d.listed_price FROM Devices d
+	LEFT OUTER JOIN Manufacturers m ON d.mfctr_id = m.mfctr_id
+	LEFT OUTER JOIN Software s ON d.soft_id = s.soft_id
+GROUP BY d.listed_price
+HAVING d.listed_price > 600.00; -- Replace 600.00 for the float value retrieved by the Java textfield
